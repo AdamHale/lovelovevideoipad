8ed2f1221eb70ac7a33517b36921fab24bc81ed5
Adam Hale (i7685032) <i7685032@mac27780.local>
2016-05-11 09:19:44 +0100

    Initial Commit

---
+++ lovelovevideoipad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lovelovevideoipad
+//
+//  Created by Adam Hale (i7685032) on 11/05/2016.
+//  Copyright © 2016 Adam Hale (i7685032). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
