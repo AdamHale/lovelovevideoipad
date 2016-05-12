e332da02e10989739d40258950c39547bbffe3db
kmcgowan0 <i7673473@Mac27779.local>
2016-05-12 15:07:06 +0100

    added cocoapods

---
+++ Pods/Target Support Files/Pods-lovelovevideoipad/Pods-lovelovevideoipad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lovelovevideoipad : NSObject
+@end
+@implementation PodsDummy_Pods_lovelovevideoipad
+@end
---
+++ lovelovevideoipad/VideoView.swift
@@ -0,0 +1,9 @@
+//
+//  VideoView.swift
+//  lovelovevideoipad
+//
+//  Created by Kathryn McGowan (i7673473) on 12/05/2016.
+//  Copyright © 2016 Adam Hale (i7685032). All rights reserved.
+//
+
+import Foundation
